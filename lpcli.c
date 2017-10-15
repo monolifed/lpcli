da8f9002c85b835352662afbe7ba2198f23cfde7
monolifed <6624464+monolifed@users.noreply.github.com>
2017-10-16 02:32:26 +0300

    update

--- lpcli.c
+++ lpcli.c
@@ -1,6 +1,11 @@
-/* HGvqQQSDvF7YlA2S */
-
+#ifndef _WIN32
+	#ifndef _XOPEN_SOURCE
+	#define _XOPEN_SOURCE
+	#endif
+	#include <termios.h>
+#endif
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -30,6 +35,19 @@ void print_usage(void)
 	fflush(stderr);
 }
 
+int print_error(const char * format, ...)
+{
+	fputs("Error: ", stderr);
+	va_list args;
+	va_start (args, format);
+	vfprintf (stderr, format, args);
+	va_end (args);
+	fflush(stderr);
+	
+	return 1;
+}
+
+
 const char *site, *login, *password;
 unsigned charset_in = 0, charset_ex = LP_CSF_ALL;
 unsigned length = 0, counter = 0;
@@ -50,7 +68,7 @@ unsigned changes = 0;
 int read_args(int argc, const char **argv)
 {
 	if(argc < 3)
-		return 0;
+		return 1;
 	int i = 0;
 	const char *ptr;
 	char *ptrEnd;
@@ -75,7 +93,7 @@ int read_args(int argc, const char **argv)
 		if(!opt_open)
 		{
 			if(*ptr != '-')
-				return 0;
+				return 1;
 			ptr++;
 		}
 		
@@ -84,14 +102,14 @@ int read_args(int argc, const char **argv)
 			case '\0': // -
 				if(!opt_open)
 				{
-					return 0;
+					return 1;
 				}
 				opt_open = 0;
 				ptr = argv[++i];
 				break;
 			case '-': // --
 				if(opt_open)
-					return 0;
+					return 1;
 				ptr++;
 				if(strcmp(ptr, "no-lowercase") == 0)
 				{
@@ -121,25 +139,25 @@ int read_args(int argc, const char **argv)
 				{
 					ptr = argv[++i];
 					if(ptr == NULL)
-						return 0;
+						return 1;
 					length = strtol(ptr, &ptrEnd, 10);
 					if(*ptrEnd != '\0')
-						return 0;
+						return 1;
 					OPTSET(LENGTH);
 				}
 				else if(strcmp(ptr, "counter") == 0)
 				{
 					ptr = argv[++i];
 					if(ptr == NULL)
-						return 0;
+						return 1;
 					counter = strtol(ptr, &ptrEnd, 10);
 					if(*ptrEnd != '\0')
-						return 0;
+						return 1;
 					OPTSET(COUNTER);
 				}
 				else
 				{
-					return 0;
+					return 1;
 				}
 				ptr = argv[++i];
 				break;
@@ -149,11 +167,11 @@ int read_args(int argc, const char **argv)
 				{
 					ptr = argv[++i];
 					if(ptr == NULL)
-						return 0;
+						return 1;
 				}
 				length = strtol(ptr, &ptrEnd, 10);
 				//if(*ptrEnd != '\0')
-				//	return 0;
+				//	return 1;
 				OPTSET(LENGTH);
 				ptr = (const char*) ptrEnd;  opt_open = 1;
 				//ptr = argv[++i];
@@ -165,11 +183,11 @@ int read_args(int argc, const char **argv)
 				{
 					ptr = argv[++i];
 					if(ptr == NULL)
-						return 0;
+						return 1;
 				}
 				counter = strtol(ptr, &ptrEnd, 10);
 				//if(*ptrEnd != '\0')
-				//	return 0;
+				//	return 1;
 				OPTSET(COUNTER);
 				ptr = (const char*) ptrEnd; opt_open = 1;
 				//ptr = argv[++i];
@@ -205,34 +223,84 @@ int read_args(int argc, const char **argv)
 				opt_open = 1;
 				break;
 			default:
-				return 0;
+				return 1;
 		}
 	}
-	return 1;
+	return 0;
 }
 #undef TOUCH
 
+
+//FILE *popen(const char *command, const char *type);
+//int pclose(FILE *stream);
+void copy_to_clipboard(const char *text)
+{
+	FILE *pout = popen("xclip -selection clipboard -quiet -loop 1", "w");
+	if(!pout)
+	{
+		fprintf(stderr, "Cannot copy to clipboard\n");
+		fflush(stderr);
+		return;
+	}
+	fprintf(pout, text);
+	fflush(pout);
+	pclose(pout);
+}
+
+int read_password(const char *prompt, char *out, size_t outl)
+{
+	printf(prompt);
+	static struct termios told, tnew;
+	tcgetattr(0, &told);
+	tnew = told;
+	tnew.c_lflag &= ~ICANON;
+	tnew.c_lflag &= ~ECHO;
+	tcsetattr(0, TCSANOW, &tnew);
+	
+	out = fgets(out, outl, stdin);
+	tcsetattr(0, TCSANOW, &told);
+	
+	if(out)
+	{
+		out[strcspn(out, "\r\n")] = 0;
+		return 0;
+	}
+	return 1;
+}
+
 #define ISOPTSET(X) (changes & CMDLINE_##X)
 
+void print_options(unsigned cs, unsigned ct, unsigned l)
+{
+	printf("Options: -");
+	if(cs & LP_CSF_LOWERCASE)
+		printf("u");
+	if(cs & LP_CSF_UPPERCASE)
+		printf("l");
+	if(cs & LP_CSF_DIGITS)
+		printf("d");
+	if(cs & LP_CSF_SYMBOLS)
+		printf("s");
+	printf("c%u", ct);
+	printf("L%u", l);
+	printf("\n");
+}
 
 int main(int argc, const char **argv)
 {
 	int ret = read_args(argc, argv);
-	if(!ret)
+	if(ret)
 	{
-		fprintf(stderr, "Unknown options specified\n");
-		fflush(stderr);
-		return 1;
+		return print_error("Unknown options specified\n");
 	}
 	
-	LP_CTX *ctx = LP_CTX_new();
 	unsigned temp;
 	if(ISOPTSET(CSETINC) && ISOPTSET(CSETEXC))
 	{
-		fprintf(stderr, "Exclusion options cannot be used with inclusion options\n");
-		fflush(stderr);
-		return 1;
+		return print_error("Exclusion options cannot be used with inclusion options\n");
 	}
+
+	LP_CTX *ctx = LP_CTX_new();
 	
 	unsigned charset;
 	if(ISOPTSET(CSETINC))
@@ -240,26 +308,20 @@ int main(int argc, const char **argv)
 		temp = LP_set_charsets(ctx, charset_in);
 		if(temp != charset_in)
 		{
-			fprintf(stderr, "Cannot set invalid charset value\n");
-			fflush(stderr);
-			return 1;
+			LP_CTX_free(ctx);
+			return print_error("Cannot set invalid charset value %u\n", charset_in);
 		}
 		charset = charset_in;
-		//fprintf(stderr, "char set to %u in options\n", charset_in);
-		//fflush(stderr);
 	}
 	else if(ISOPTSET(CSETEXC))
 	{
 		temp = LP_set_charsets(ctx, charset_ex);
 		if(temp != charset_ex)
 		{
-			fprintf(stderr, "Cannot set invalid charset value\n");
-			fflush(stderr);
-			return 1;
+			LP_CTX_free(ctx);
+			return print_error("Cannot set invalid charset value %u\n", charset_ex);
 		}
 		charset = charset_ex;
-		//fprintf(stderr, "nchar set to %u in options\n", charset_ex);
-		//fflush(stderr);
 	}
 	else
 	{
@@ -271,13 +333,9 @@ int main(int argc, const char **argv)
 		temp = LP_set_length(ctx, length);
 		if(temp != length)
 		{
-			fprintf(stderr, "Cannot set invalid length value\n");
-			fflush(stderr);
-			return 1;
+			LP_CTX_free(ctx);
+			return print_error("Cannot set invalid length value %u\n", length);
 		}
-		//fprintf(stderr, "length set to %u in options\n", length);
-		//fflush(stderr);
-		//length = temp;
 	}
 	else
 	{
@@ -289,36 +347,47 @@ int main(int argc, const char **argv)
 		temp = LP_set_counter(ctx, counter);
 		if(temp != counter)
 		{
-			fprintf(stderr, "Cannot set invalid counter value\n");
-			fflush(stderr);
-			return 1;
+			LP_CTX_free(ctx);
+			return print_error("Cannot set invalid counter value %u\n", counter);
 		}
-		//fprintf(stderr, "counter set to %u in options\n", counter);
-		//fflush(stderr);
-		//counter = temp;
 	}
 	else
 	{
 		counter = LP_set_counter(ctx, 0);
 	}
 	
-	if(!ISOPTSET(PASSWORD))
-	{
-		fprintf(stderr, "Currently password must be specified in the commandline\n");
-		fflush(stderr);
-		return 1;
-	}
-	if(ISOPTSET(CLIPBOARD))
-	{
-		fprintf(stderr, "Currently clipboard is not supported\n");
-		fflush(stderr);
-	}
-	
-	
 	char genpass[length + 1];
 	genpass[length] = 0;
-	LP_get_pass(ctx, site, login, password, genpass, sizeof genpass);
-	printf("%s\n", genpass);
+	
+	print_options(charset, counter, length);
+	
+	if(!ISOPTSET(PASSWORD))
+	{
+
+		char passwd_in[1024];
+		if(read_password("Password: ", passwd_in, sizeof passwd_in))
+		{
+			LP_CTX_free(ctx);
+			return print_error("Cannot read password\n");
+		}
+		LP_get_pass(ctx, site, login, (const char *) passwd_in, genpass, sizeof genpass);
+	}
+	else
+	{
+		LP_get_pass(ctx, site, login, password, genpass, sizeof genpass);
+	}
+	
 	LP_CTX_free(ctx);
+	
+	if(ISOPTSET(CLIPBOARD))
+	{
+		copy_to_clipboard(genpass);
+	}
+	else
+	{
+		printf("%s\n", genpass);
+	}
+	
+	
 	return 0;
 }
